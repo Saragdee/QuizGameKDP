4cfbdf4d77ea50ab5e088caa093337924be1b422
AmberEdgaras <edasmackis@gmail.com>
2024-08-31 15:43:27 +0300

    AnswerFeedbackManager

---
+++ Assets/Scripts/manager/AnswerFeedbackManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace manager
+{
+    public class AnswerFeedbackManager : MonoBehaviour
+    {
+        public Animator correctPanelAnimator;
+        public Animator wrongPanelAnimator;
+
+        public void ShowCorrectAnswer()
+        {
+            correctPanelAnimator.SetTrigger("ShowCorrect");
+        }
+
+        public void ShowWrongAnswer()
+        {
+            wrongPanelAnimator.SetTrigger("ShowWrong");
+        }
+    }
+}
